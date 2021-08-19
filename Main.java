bd5ac9556f6cd35b47db4f425816ffa2c0071655
RagimovRafshan <89220763+RagimovRafshan@users.noreply.github.com>
2021-08-20 01:22:16 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
-        for(int i = 0 ; i < 20; i++) {
+        while(true) {
             String str = scan.next();
             System.out.println(calc.toSalve(str));
         }
